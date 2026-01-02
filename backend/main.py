fb02f26b4d525f6f73a5d8602184949075d1d76b
Tuan Nguyen <anhtuannguyentran101006@gmail.com>
2026-01-01 16:55:11 -0800

    add prompts back

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,3 @@
-import os
-from pathlib import Path
-
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -9,13 +6,12 @@ from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 from langchain.agents import create_agent
-
+from langchain.agents.structured_output import ProviderStrategy
 
 from tools import search_tool, wiki_tool, save_tool
 
-
-
 load_dotenv()
+
 llm = ChatOpenAI(model="gpt-5-nano")
 
 
@@ -30,17 +26,27 @@ class UserQuery(BaseModel):
     query: str
 
 
+SYSTEM_PROMPT = """
+You are a research assistant that will help generate a research paper. You need to be somehow funny but informational
+
+Rules:
+- Use tools when useful.
+- Be concise and factual.
+- Put sources as URLs in the sources list.
+- Fill tools_used with the tool names you called.
+"""
+
 agent = create_agent(
     model=llm,
     tools=[search_tool, wiki_tool, save_tool],
-    response_format=ResearchResponse,
+    system_prompt=SYSTEM_PROMPT,  
+    response_format=ProviderStrategy(ResearchResponse),  
 )
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -49,8 +55,5 @@ app.add_middleware(
 
 @app.post("/api/research", response_model=ResearchResponse)
 async def run_research(user_input: UserQuery):
-    state = {"messages": [HumanMessage(content=user_input.query)]}
-
-    result = await agent.ainvoke(state)
-
+    result = agent.invoke({"messages": [HumanMessage(content=user_input.query)]})
     return result["structured_response"]
